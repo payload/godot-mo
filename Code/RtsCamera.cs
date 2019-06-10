e9cfe58df241905d12b6861d598115b0c599c346
Gilbert Roehrbein <gilbert.rohrbein@logmein.com>
2019-06-10 15:55:43 +0200

    fix camera movement

--- Code/RtsCamera.cs
+++ Code/RtsCamera.cs
@@ -8,15 +8,18 @@ public class RtsCamera : Camera
         var margin = 50;
         var viewport = GetViewport();
         var pos = viewport.GetMousePosition();
-        
+        var dir = Vector3.Zero;
+
         if (pos.x < margin && pos.x > 0)
-            HOffset -= delta * 3;
+            dir += Vector3.Left;
         if (pos.x > -margin + viewport.Size.x && pos.x < viewport.Size.x)
-            HOffset += delta * 3;
+            dir += Vector3.Right;
         if (pos.y < margin && pos.y > 0)
-            VOffset += delta * 3;
+            dir += Vector3.Forward;
         if (pos.y > -margin + viewport.Size.y && pos.y < viewport.Size.y)
-            VOffset -= delta * 3;
+            dir += Vector3.Back;
+        
+        GlobalTranslate(dir.Normalized() * delta * 6);
     }
 
     public override void _UnhandledInput(InputEvent @event) {
