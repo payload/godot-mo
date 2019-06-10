ec0dfc015d13bdac792d6c1a66a752165b23dbff
Gilbert Roehrbein <gilbert.rohrbein@logmein.com>
2019-06-10 22:05:07 +0200

    removed unused code

--- Code/Block.gd
+++
@@ -1,9 +0,0 @@
-extends RigidBody
-
-export(Material) onready var material = null setget set_material, get_material
-
-func set_material(mat):
-	$MeshInstance.set_surface_material(0, mat)
-
-func get_material():
-	return $MeshInstance.get_surface_material(0)
--- Code/LookAtCamera.gd
+++
@@ -1,9 +0,0 @@
-extends Spatial
-
-var cam: Spatial
-
-func _ready():
-	cam = get_node("/root/World/Camera")
-
-func _process(_delta):
-	transform = transform.looking_at(cam.translation, Vector3.UP)
